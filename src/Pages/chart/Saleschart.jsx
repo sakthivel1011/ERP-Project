4ffef843c65343405970018eb8b391af97e537f0
veeramani <veeradk30.01@gmail.com>
2026-08-07 11:22:12 +0530

    change div

--- src/Pages/chart/Saleschart.jsx
+++ src/Pages/chart/Saleschart.jsx
@@ -7,7 +7,9 @@ export default function Saleschart({chartData}) {
    <BarChart className='barchart' width={600} height={300} margin={{top:20,right:30,left:200,bottom:5}} data={chartData}>
       <XAxis dataKey="month" />
       <YAxis dataKey="sales"/>                 
-      <Tooltip />              
+      <Tooltip />  
+      <div>
+        </div>            
       <Bar dataKey="sales" fill="blue" radius={[6,6,0,0]}/> 
     </BarChart>
 
